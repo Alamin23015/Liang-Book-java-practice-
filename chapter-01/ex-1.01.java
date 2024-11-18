1e1abd7e4ab78c941d235bc1425b41142d7565c8
Alamin23015 <alamin16105@gmail.com>
2024-11-18 21:31:12 +0600

    ex-1.01.java

---
+++ chapter-01/ex-1.01.java
@@ -0,0 +1,8 @@
+//alamin23015
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java");
+        System.out.println("Welcome To ICT");
+        System.out.println("Programming is fun!");
+    }
+}
