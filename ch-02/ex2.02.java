bc2654edee1f830043d176e2fa03b7b56e8652b8
Alamin23015 <alamin16105@gmail.com>
2024-11-18 22:02:44 +0600

    ex2.02.java

---
+++ ch-02/ex2.02.java
@@ -0,0 +1,9 @@
+//scale change
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double pound=input.nextDouble();
+        System.out.println(pound+ " Pound  = " + pound/0.454 + "Kilograms");
+    }
+}
